1a54849814c8b275bcf7f27a51b82e2d927ee12b
Joe Diamand <jdiamand@yahoo.com>
2024-01-31 21:27:12 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# cop6481_script_1
+Spring 2024 Computational Methods in FinTech 
+<br>
+** Script 1 Team6**
+
+
+## Transaction Network Analysis
+
+### Problem Statement:
+
+In a financial transaction network<br>
+* each node represents a user, and 
+* each directed edge represents a financial transaction from one user to another. 
+
+Write a Python program to perform the following tasks:
+* Load Data:<br>
+    Read a CSV file containing transaction data.<br>
+    The CSV file should have two columns: 'sender' and 'receiver,'<br>
+    representing the sender and receiver of each transaction.<br>
+    You can create your data file with 10-15 users(nodes).<br>
+**_Note:_**<br>
+
+        Our .csv file is called transaction. 
+        It contains 3 columns, the required 'sender' and 'receiver' columns and  
+        a 3rd column called amount. 
+        Our program will use the 'Amount' column to satisfy the 
+        some of the additional requirements, below. 
+
+* Build a Graph:<br>
+  Create a directed graph where nodes represent users and edges represent transactions.<br>
+**_Note:_**<br>
+
+        The column 'Amount. 
+        Is used as the value (weight) on the edge between two nodes and 
+        represents the value of a payment from one person (the source node) to
+        another (the destination node)
+
+* Detect Fraudulent Transactions:
+  Implement a function to detect potentially fraudulent transactions.<br> 
+  You can define fraud based on a certain threshold of transaction amounts or <br>
+  other relevant criteria.Print the details of any detected fraudulent transactions,<br>
+  including the sender, receiver, and transaction amount.<br>
+**_Note:_**<br>
+
+        We defined potential fraud (or money laundering) as any payment amount 
+        over $50.00.
+
+* Calculate Degree Centrality: (Bonus - 5 points)<br>
+  Implement a function to calculate the degree of centrality<br>
+  of each user in the network.<br>
+  Print the top N users with the highest degree of centrality.
+
+* Visualize the Transaction Network:<br>
+  Use a graph visualization library (e.g., NetworkX, Matplotlib)<br>
+  to visualize the transaction network.
+
+
+## Running the program
+
+The entire code is contained in file <br>
+"network_transactions.py"
+To run the program simply run this file.
---
+++ network_transactions.py
@@ -0,0 +1,137 @@
+import csv
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+# ########################################
+# Spring 2024 Computational Methods in FinTech
+# Script 1
+# Team 6
+# ########################################
+#
+# #####
+# the read_transactions function
+#   input : a file name
+#   output : the transaction graph as an nx.DiGraph object
+#   We use the NetworkX library  to create and work with our directed graph.
+#   Function explanation:
+#       We call nx.DiGraph() to create our directed graph, "transaction_graph"
+#       Then we read the contents of the csv file
+#       For each line on the input csv file we connect a weighted, directed edge between
+#       the sender and receiver.
+#       when finished reading the csv file we retunr the NetworkX grapg to the caller
+# #####
+def read_transactions(file_name):
+    transaction_graph = nx.DiGraph()
+    with open(file_name, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader, None)  # Skip the first row that contains header info
+        for row in reader:  # Process each remaining row
+            # extract the transaction amount and the sender/receiver from
+            # the input line just read
+            amount, sender, receiver = float(row[0]), row[1], row[2]
+            # use the nx.add_edge call to create an edge with the "amount"
+            # the add add_edge() call adds an edge to the graph "transaction_graph"
+            # from the node "sender" to the node "receiver",
+            # and it associates this edge with some data ... in this case, amount.
+            # amount is treated as an attribute of the edge connecting sender to receiver.
+            transaction_graph.add_edge(sender, receiver, amount=amount)
+    return transaction_graph
+
+
+#
+# #####
+# the visualize_network function
+#   input : graoh - this is the networkx object that contains all
+#                   the nodes and edges for from our transactions.cvs file
+#           fraud_edges - these are the edges that represent potential
+#                   fraud that were detected in the fraud_detection function
+#   output : this function output a graph of the transactions.cvs data
+def visualize_network(graph, fraud_edges=None):
+    # Compute the layout
+    if fraud_edges is None:
+        fraud_edges = []
+
+#       compute the layout for the graph using the Kamada-Kawai layout algorithm.
+#       this layout algorithm will position nodes in the graph in an aesthetically pleasing way.
+    pos = nx.kamada_kawai_layout(graph)
+
+    # Node sizes based on degree (slightly larger)
+    # the alpha value makes the nodes slightly transparent
+    degrees = [val for (node, val) in graph.degree()]
+    nodes = nx.draw_networkx_nodes(graph, pos, node_size=[v * 300 for v in degrees], alpha=0.9)
+    # create lablels for each node to be added to the graph
+    labels = {node: node for node in graph.nodes()}
+    nx.draw_networkx_labels(graph, pos, labels, font_size=14)
+
+    # differentualate Normal edges from fraud edges
+    normal_edges = [(u, v) for u, v in graph.edges() if (u, v) not in fraud_edges]
+    # label the non-fraud edges
+    normal_edge_labels = {(u, v): f"${graph[u][v]['amount']:.2f}" for u, v in normal_edges}
+    # use the graph , the pos calculated above, draw thw non-fraud edges with a blue color
+    nx.draw_networkx_edges(graph, pos, edgelist=normal_edges, edge_color='blue', width=1.0)
+
+
+    # now draw the lable for the non-fraud edges
+    # place the label for the amount slightly above the line for better visability
+    edge_label_pos_offset = -0.075
+    for (u, v), label in normal_edge_labels.items():
+        x = (pos[u][0] + pos[v][0]) / 2  # Midpoint x-coordinate
+        y = (pos[u][1] + pos[v][1]) / 2 + edge_label_pos_offset  # Midpoint y-coordinate + offset
+        plt.text(x, y, label, color='blue', ha='center', va='bottom')  # Adjust the vertical alignment
+
+    # Now draw the Fraud edges
+    # Get the list of labels for the fraud edges
+    fraud_edge_labels = {(u, v): f"${graph[u][v]['amount']:.2f}" for u, v in fraud_edges}
+    # draw the fraud edges,m slightly thicket and with a red color
+    nx.draw_networkx_edges(graph, pos, edgelist=fraud_edges, edge_color='red', width=2.0)
+
+    # Adjust positions for fraud edge labels (slightly above the line)
+    # add the text for the amount to the fraud edges
+    for (u, v), label in fraud_edge_labels.items():
+        x = (pos[u][0] + pos[v][0]) / 2  # Midpoint x-coordinate
+        y = (pos[u][1] + pos[v][1]) / 2 + edge_label_pos_offset  # Midpoint y-coordinate + offset
+        plt.text(x, y, label, color='red', ha='center', va='bottom')  # Adjust the vertical alignment
+    # remove axis
+    plt.axis('off')
+    # render the graph
+    plt.show()
+
+
+#
+# #####
+# the fraud_detection function
+#   input : graoh - this is the networkx object that contains all
+#                   the nodes and edges for from our transactions.cvs file
+#           threshold - any transaction that exceeds this ammount will be flagged as
+#                    potential fraud. The default is $500.00
+#   output : the fraud_edges , this will be used by the visualization function
+def fraud_detection(graph, threshold=500.00):
+    #   find the list of edges that represent transactions greater
+    #       than the threshold
+    fraud_edges = [(u, v) for u, v in graph.edges() if graph[u][v]['amount'] > threshold]
+    #   print the list of fraud edges:   the source person, destination person,
+    #       and the transaction amount
+    for sender, receiver in fraud_edges:
+        amount = graph[sender][receiver]['amount']
+        print(f"Potential Fraud Detected (Amount Exceeds $500.00): {sender} -> {receiver}, Amount: ${amount:.2f}")
+    #   return the list of fraud edges that were found to caller
+    return fraud_edges
+
+
+#
+# #####
+# the main function
+#       step 1 : read in the transactions.cvs file and contruct the
+#               graph whose nodes are the persons and the directed edges
+#               are payments from one person to another
+#       step 2 : detect the list of fraud transactions,
+#                   this will be the list of edges whose values exceed the
+#                   threshold ammount. print out this list.
+#       step 3 : visualize the network using matplotlib.pyplot
+#
+#
+if __name__ == "__main__":
+    graph = read_transactions('transactions.csv')
+    fraud_edges = fraud_detection(graph, 500.00)
+    visualize_network(graph, fraud_edges)
